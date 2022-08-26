a8d14dbc9c44ef0c63c7f179921b83c49921ce57
kunalshaw79 <95361750+kunalshaw79@users.noreply.github.com>
2022-08-26 20:15:32 +0530

    Update Home.js
    adjusting the response after changing response from api

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import './component.css';
 import { useState } from "react";
 const Home = () => {
-  const profile = JSON.parse(localStorage.getItem('profile'));
+//   const profile = JSON.parse(localStorage.getItem('profile')); no use
   const ID =localStorage.getItem('token');
   const fetchProfile= async()=>{
     const response = await fetch("http://localhost:5000/api/auth/getuser", {
@@ -15,10 +15,11 @@ const Home = () => {
   const res = await response.json();
   console.log(res);
   document.getElementById('box').innerHTML =`
-  <h1>welcome ${res.name}</h1>
-  <h2>your age is ${profile.age}</h2>
-  <h2>your phone number is ${profile.mobile}</h2>
-  <h2>you are ${profile.gender}</h2>`
+  <h1>welcome ${res[0].name}</h1>
+  <h2>your email is ${res[0].email}</h2>
+  <h2>your age is ${res[1].age}</h2>
+  <h2>your phone number is ${res[1].mobile}</h2>
+  <h2>you are ${res[1].gender}</h2>`
   }
   fetchProfile()
     return (
